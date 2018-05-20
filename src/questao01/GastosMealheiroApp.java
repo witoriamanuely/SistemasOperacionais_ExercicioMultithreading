9a64ee1d34d5101757cb55d843d74a3e6e9cf784
witoriamanuely <manuelycavalcante@gmail.com>
2018-05-20 19:25:17 -0300

    Implementando Thread de forma que a discussão fique paralela.

--- src/questao01/GastosMealheiroApp.java
+++ src/questao01/GastosMealheiroApp.java
@@ -2,11 +2,16 @@ package questao01;
 
 public class GastosMealheiroApp 
 {
-	public static void main(String[] args) 
-	{	Homem homem = new Homem();
-		homem.informarDesejo();
-		Mulher mulher = new Mulher();
-		mulher.informarDesejo();
-		System.out.println("O ganhador � quem falou por �ltimo");
+	public static void main(String[] args) throws InterruptedException {
+        Mulher mulher = new Mulher();
+        Homem homem = new Homem();
+        Thread rodaMulher = new Thread(mulher);
+        Thread rodaHomem = new Thread(homem);
+
+        rodaHomem.start();
+        rodaMulher.start();
+        rodaHomem.join();
+        rodaMulher.join();
+		System.out.println("O ganhador eh quem falou por ultimo");
 	}
 }
