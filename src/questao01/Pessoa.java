57212abc40e5484cd142d23d0bc4d1f0488a76a2
witoriamanuely <manuelycavalcante@gmail.com>
2018-05-20 19:28:03 -0300

    Implementando Runnable.

--- src/questao01/Pessoa.java
+++ src/questao01/Pessoa.java
@@ -1,9 +1,9 @@
 package questao01;
 
-public abstract class Pessoa {
+public abstract class Pessoa implements Runnable {
 	private static final int QTD_ITERACAO = 10;
 	private static final int ESPERA = 1000;
-	
+	private static String desejo = "";
 	public void informarDesejo() {
 		for (int i = 0; i < QTD_ITERACAO; i++){
 		    System.out.println(getDesejo());
@@ -15,7 +15,10 @@ public abstract class Pessoa {
 	}
 
 	public abstract String getDesejo();
-	
+
+	public void run(){
+        this.informarDesejo();
+    }
 	private long getEspera(){
         return (long)(Math.random() * ESPERA);
 	}
