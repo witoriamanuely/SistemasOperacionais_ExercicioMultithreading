f46d8ec71a86ec740cb84d9be38c0c0783d9e5d3
AndersonNobrega <anderson.nobrega62@gmail.com>
2018-05-25 01:29:36 -0300

    Adicionando as questões 3 e 5 com thread

--- src/questao03/PortaRetratoDigitalApp.java
+++ src/questao03/PortaRetratoDigitalApp.java
@@ -6,6 +6,8 @@ public class PortaRetratoDigitalApp
 
 	public static void main(String[] args) throws InterruptedException
     {   String homePath = System.getProperty("user.home");
-    	PortaRetrato portaRetrato = new PortaRetrato(homePath + System.getProperty("file.separator") + "Pictures", TIME_OUT);	   
+    	PortaRetrato portaRetrato = new PortaRetrato(homePath + System.getProperty("file.separator") + "Imagens", TIME_OUT);
+    	Thread portaRetratoThread = new Thread(portaRetrato);
+    	portaRetratoThread.start();
     }
 }
